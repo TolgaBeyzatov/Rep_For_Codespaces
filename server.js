6ae6c3e8f45afb04d28ac8bbe6de6b685905f095
TolgaBeyzatov <115338509+TolgaBeyzatov@users.noreply.github.com>
2024-03-07 09:09:08 +0000

    Changed Startup Message again

--- server.js
+++ server.js
@@ -30,4 +30,4 @@ app.use(dbSetup);
 app.use('/', routes);
 
 // Server
-app.listen(port, () => console.log(`Server now running on :${port}`));
+app.listen(port, () => console.log(`Server is running on :${port}`));
